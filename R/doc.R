9ac8ed9b3e8f6843facfc39501795ab96bc4d648
sokovalevsky <sokovalevsky@gmail.com>
2017-03-26 00:31:32 +0300

    fasttime import added

--- R/doc.R
+++ R/doc.R
@@ -21,6 +21,7 @@
 #' @importFrom utils read.delim tail capture.output download.file unzip
 #' @importFrom Rcpp evalCpp
 #' @importFrom R6 R6Class
+#' @importFrom fasttime fastPOSIXct
 #' @import data.table
 #' @useDynLib QuantTools, .registration = TRUE
 NULL
